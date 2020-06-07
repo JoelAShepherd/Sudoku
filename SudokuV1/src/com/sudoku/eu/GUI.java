f54db0f57991013a2cd290e66800823ad28524bb
JShep <66128412+JoelAShepherd@users.noreply.github.com>
2020-06-07 16:16:12 +0100

    First try to use GridBagLayout to make GUI.

--- SudokuV1/src/com/sudoku/eu/GUI.java
+++ SudokuV1/src/com/sudoku/eu/GUI.java
@@ -1,23 +1,74 @@
 package com.sudoku.eu;
 
+
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+
+import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
-import javax.swing.SpringLayout;
+import javax.swing.SwingUtilities;
 
 
 public class GUI {
+	JFrame frame = new JFrame();
+	JPanel panel = new JPanel();
+	JTextField jtf0 = new JTextField(2);
+	JTextField jtf1 = new JTextField(2);
+	JTextField jtf2 = new JTextField(2);
+	JTextField jtf3 = new JTextField(2);
+	JTextField jtf4 = new JTextField(2);
+	JTextField jtf5 = new JTextField(2);
+	JTextField jtf6 = new JTextField(2);
+	JTextField jtf7 = new JTextField(2);
+	JTextField jtf8 = new JTextField(2);
+	
+	
+	
+public GUI() {
+	
+	panel.setLayout(new GridBagLayout());
+	GridBagConstraints gbc = new GridBagConstraints();
+	
+	gbc.gridwidth = 1;
+	gbc.gridheight = 1; 
+	gbc.weightx = +1.;
+	gbc.ipadx = 1;
+	
+	
+	gbc.gridx = 0;
+	gbc.gridy = 0;
+	panel.add(jtf0, gbc);
+	
+	gbc.gridx = 1;
+	gbc.gridy = 0;
+	panel.add(jtf1, gbc);
+	
+	gbc.gridx = 2;
+	gbc.gridy = 0;
+	panel.add(jtf2, gbc);
+	
+	gbc.gridx = 0;
+	gbc.gridy = 1;
+	panel.add(jtf8);
+	
+	
+	
+	frame.add(panel);
+	frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+	frame.pack();
+	frame.setVisible(true);
+	
+}
+	
+	
 	public static void main(String args[]) {
-		JPanel panel = new JPanel(new SpringLayout());
-
-		int rows = 9;
-		int cols = 9;
-		for (int r = 0; r < rows; r++) {
-			for (int c = 0; c < cols; c++) {
-				int anInt = (int) Math.pow(r, c);
-				JTextField textField = new JTextField(Integer.toString(anInt));
-				panel.add(textField);
+		SwingUtilities.invokeLater(new Runnable() {
+			@Override 
+			public void run() {
+				new GUI();
 			}
-			
-		}
+		});
+
 	}
 }
